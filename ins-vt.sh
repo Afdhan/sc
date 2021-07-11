2bdfed02163c7a80b86dee7600ea6743f7b9438d
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-07-11 08:53:55 +0700

    F

--- ins-vt.sh
+++ ins-vt.sh
@@ -490,34 +490,7 @@ systemctl restart trojan
 systemctl enable trojan
 systemctl restart v2ray
 systemctl enable v2ray
-cd /usr/bin
-wget -O add-ws "https://raw.githubusercontent.com/Afdhan/sc/main/add-ws.sh"
-wget -O add-vless "https://raw.githubusercontent.com/Afdhan/sc/main/add-vless.sh"
-wget -O add-tr "https://raw.githubusercontent.com/Afdhan/sc/main/add-tr.sh"
-wget -O del-ws "https://raw.githubusercontent.com/Afdhan/sc/main/del-ws.sh"
-wget -O del-vless "https://raw.githubusercontent.com/Afdhan/sc/main/del-vless.sh"
-wget -O del-tr "https://raw.githubusercontent.com/Afdhan/sc/main/del-tr.sh"
-wget -O cek-ws "https://raw.githubusercontent.com/Afdhan/sc/main/cek-ws.sh"
-wget -O cek-vless "https://raw.githubusercontent.com/Afdhan/sc/main/cek-vless.sh"
-wget -O cek-tr "https://raw.githubusercontent.com/Afdhan/sc/main/cek-tr.sh"
-wget -O renew-ws "https://raw.githubusercontent.com/Afdhan/sc/main/renew-ws.sh"
-wget -O renew-vless "https://raw.githubusercontent.com/Afdhan/sc/main/renew-vless.sh"
-wget -O renew-tr "https://raw.githubusercontent.com/Afdhan/sc/main/renew-tr.sh"
-wget -O certv2ray "https://raw.githubusercontent.com/Afdhan/sc/main/cert.sh"
-chmod +x add-ws
-chmod +x add-vless
-chmod +x add-tr
-chmod +x del-ws
-chmod +x del-vless
-chmod +x del-tr
-chmod +x cek-ws
-chmod +x cek-vless
-chmod +x cek-tr
-chmod +x renew-ws
-cmod +x renew-vless
-chmod +x renew-tr
-chmod +x certv2ray
-cd
+figlet M AFDHAN  NezaVPN | lolcat
 rm -f ins-vt.sh
 mv /root/domain /etc/v2ray
 

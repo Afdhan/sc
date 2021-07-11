555ca49c56976d0d92b209a72bdcf9b1a93e39a2
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-07-11 08:56:48 +0700

    F

--- sodosok.sh
+++ sodosok.sh
@@ -87,14 +87,5 @@ iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 2443:3543 -j ACCEPT
 iptables -I INPUT -m state --state NEW -m udp -p udp --dport 2443:3543 -j ACCEPT
 iptables-save > /etc/iptables.up.rules
 ip6tables-save > /etc/ip6tables.up.rules
-cd /usr/bin
-wget -O add-ss "https://raw.githubusercontent.com/Afdhan/sc/main/add-ss.sh"
-wget -O del-ss "https://raw.githubusercontent.com/Afdhan/sc/main/del-ss.sh"
-wget -O cek-ss "https://raw.githubusercontent.com/Afdhan/sc/main/cek-ss.sh"
-wget -O renew-ss "https://raw.githubusercontent.com/Afdhan/sc/main/renew-ss.sh"
-chmod +x add-ss
-chmod +x del-ss
-chmod +x cek-ss
-chmod +x renew-ss
-cd
+figlet M AFDHAN  NezaVPN | lolcat
 rm -f /root/sodosok.sh

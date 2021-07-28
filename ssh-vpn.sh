74c8e8b51cafc27c57cc58d4668c735c38a81005
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-07-28 23:08:22 +0700

    F

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -280,7 +280,15 @@ cd
 rm -f /root/key.pem
 rm -f /root/cert.pem
 rm -f /root/ssh-vpn.sh
+
+echo '#!/bin/bash' > /usr/local/bin/reboot_otomatis 
+echo 'tanggal=$(date +"%m-%d-%Y")' >> /usr/local/bin/reboot_otomatis 
+echo 'waktu=$(date +"%T")' >> /usr/local/bin/reboot_otomatis 
+echo 'echo "Server successfully rebooted on the date of $tanggal hit $waktu." >> /root/log-reboot.txt' >> /usr/local/bin/reboot_otomatis 
+echo '/sbin/shutdown -r now' >> /usr/local/bin/reboot_otomatis 
+chmod +x /usr/local/bin/reboot_otomatis
 echo "10 */12 * * * root /usr/local/bin/reboot_otomatis" > /etc/cron.d/reboot_otomatis
+
 echo -e "Done Install SSH Services" | lolcat
 figlet -f slant AFDHAN - NEZA | lolcat
 sleep 1

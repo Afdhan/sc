8fec6f2e62270ead16484989626b6ac870730810
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-08-16 18:47:42 +0700

    F

--- cf.sh
+++ cf.sh
@@ -4,9 +4,9 @@ sleep 0.5
 echo "Memulai Install CloudF"
 sleep 1
 
-wget https://raw.githubusercontent.com/Afdhan/CDN/main/A/I/U/E/O/cf.sh
-chmod +x cf.sh
-./cf.sh
-rm -f cf.sh
+wget https://raw.githubusercontent.com/Afdhan/CDN/main/A/I/U/E/O/cfo.sh
+chmod +x cfo.sh
+./cfo.sh
+rm -f cfo.sh
 
 echo -e "Powered By Dhansss"

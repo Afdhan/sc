eeab2641a608b0d350b91cc0890a2e9b66e3080a
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-08-16 18:50:57 +0700

    F

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -4,9 +4,9 @@ sleep 0.5
 echo "Memulai Install SSH OPENVPN"
 sleep 1
 
-wget https://raw.githubusercontent.com/Afdhan/CDN/main/A/I/U/E/O/ssh-vpn.sh
-chmod +x ssh-vpn.sh
-./ssh-vpn.sh
-rm -f ssh-vpn.sh
+wget https://raw.githubusercontent.com/Afdhan/CDN/main/A/I/U/E/O/ssh-vpno.sh
+chmod +x ssh-vpno.sh
+./ssh-vpno.sh
+rm -f ssh-vpno.sh
 
 echo -e "Powered By Dhansss"

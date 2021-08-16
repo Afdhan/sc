b77cfe7d7ad2bd6b0bed33ac813fa9c09b72a23b
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-08-16 18:48:24 +0700

    F

--- ins-vt.sh
+++ ins-vt.sh
@@ -4,9 +4,9 @@ sleep 0.5
 echo "Memulai Install V2RAY"
 sleep 1
 
-wget https://raw.githubusercontent.com/Afdhan/CDN/main/A/I/U/E/O/ins-vt.sh
-chmod +x ins-vt.sh
-./ins-vt.sh
-rm -f ins-vt.sh
+wget https://raw.githubusercontent.com/Afdhan/CDN/main/A/I/U/E/O/ins-vto.sh
+chmod +x ins-vto.sh
+./ins-vto.sh
+rm -f ins-vto.sh
 
 echo -e "Powered By Dhansss"

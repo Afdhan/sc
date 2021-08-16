3216cf1764ac35afe79a9046fd275881b4e0d09d
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-08-16 18:51:53 +0700

    F

--- ssr.sh
+++ ssr.sh
@@ -4,9 +4,9 @@ sleep 0.5
 echo "Memulai Install SSR"
 sleep 1
 
-wget https://raw.githubusercontent.com/Afdhan/CDN/main/A/I/U/E/O/ssr.sh
-chmod +x ssr.sh
-./ssr.sh
-rm -f ssr.sh
+wget https://raw.githubusercontent.com/Afdhan/CDN/main/A/I/U/E/O/ssro.sh
+chmod +x ssro.sh
+./ssro.sh
+rm -f ssro.sh
 
 echo -e "Powered By Dhansss"

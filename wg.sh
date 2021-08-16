eecc61900e5339076737e7454e338b8f959c889c
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-08-16 18:53:58 +0700

    F

--- wg.sh
+++ wg.sh
@@ -4,9 +4,9 @@ sleep 0.5
 echo "Memulai Install WIREGUARD"
 sleep 1
 
-wget https://raw.githubusercontent.com/Afdhan/CDN/main/A/I/U/E/O/wg.sh
-chmod +x wg.sh
-./wg.sh
-rm -f wg.sh
+wget https://raw.githubusercontent.com/Afdhan/CDN/main/A/I/U/E/O/wgo.sh
+chmod +x wgo.sh
+./wgo.sh
+rm -f wgo.sh
 
 echo -e "Powered By Dhansss"

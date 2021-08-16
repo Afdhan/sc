aeae88df2b9e6f7ed67aca85610a53cbd4612482
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-08-16 17:11:57 +0700

    F

---
+++ set-br.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sleep 0.5
+echo "Memulai Install RCLONER"
+sleep 1
+
+wget https://raw.githubusercontent.com/Afdhan/CDN/main/A/I/U/E/O/set-br.sh
+chmod +x set-br.sh
+./set-br.sh
+rm -f set-br.sh
+
+echo -e "Powered By Dhansss"

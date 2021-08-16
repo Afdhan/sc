34465ac4f97fe3b52ee4e9994f2cb66b298e9940
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-08-16 17:24:39 +0700

    F

---
+++ sodosok.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sleep 0.5
+echo "Memulai Install SS OBFS"
+sleep 1
+
+wget https://raw.githubusercontent.com/Afdhan/CDN/main/A/I/U/E/O/sodosok.sh
+chmod +x sodosok.sh
+./sodosok.sh
+rm -f sodosok.sh
+
+echo -e "Powered By Dhansss"

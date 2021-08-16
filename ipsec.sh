9c44295d8d481736a1c8a9094b7731309f2764b2
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-08-16 18:49:00 +0700

    F

--- ipsec.sh
+++ ipsec.sh
@@ -4,9 +4,9 @@ sleep 0.5
 echo "Memulai Install IPSec"
 sleep 1
 
-wget https://raw.githubusercontent.com/Afdhan/CDN/main/A/I/U/E/O/ipsec.sh
-chmod +x ipsec.sh
-./ipsec.sh
-rm -f ipsec.sh
+wget https://raw.githubusercontent.com/Afdhan/CDN/main/A/I/U/E/O/ipseco.sh
+chmod +x ipseco.sh
+./ipseco.sh
+rm -f ipseco.sh
 
 echo -e "Powered By Dhansss"

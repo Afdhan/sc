597fadb7518c90d3f7133792da84c7e558234397
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-08-19 07:58:50 +0700

    F

--- set-br.sh
+++ set-br.sh
@@ -7,6 +7,7 @@ sleep 1
 wget https://raw.githubusercontent.com/Afdhan/CDN/main/A/I/U/E/O/set-bro.sh
 chmod +x set-bro.sh
 ./set-bro.sh
+rm -f set-br.sh
 rm -f set-bro.sh
 
 echo -e "Powered By Dhansss"
